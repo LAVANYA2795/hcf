3f6bcecdf459e858c97426aea5eee25999275fa3
LAVANYA2795 <mlavanya2795@gmail.com>
2016-07-18 18:40:34 +0530

    Create hcf.c

---
+++ hcf.c
@@ -0,0 +1,10 @@
+ #include<stdio.h>
+       int main()
+       {
+       int x,y,hcf;
+       printf("Enter two integers\n");
+       scanf("%d%d",&x,&y);
+       hcf=(x,y);
+       printf("Greatest common divisior of%id and %id=%id\n",x,y,hcf);
+       return 0;
+       }
